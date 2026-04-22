91940bfb45a0ef4fb61dbe2013daefeff480e58a
anil-kumar981 <yadavanilkumar8113@gmail.com>
2026-04-22 10:48:20 +0530

    Super admin and orm models

---
+++ admin_super_user/portfolio/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    portfolio = models.ForeignKey(Portfolio, related_name='projects', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
